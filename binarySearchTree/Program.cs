eb3e7bdc7eacf1cd8f2a739bf1188d664eaa648e
“sarthak <sarthakgangrade0@gmail.com>
2021-12-02 01:58:19 +0530

    create a binary search tree

---
+++ binarySearchTree/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BST tree = new BST();
+            tree.Insert(65);
+            tree.Insert(40);
+            tree.Insert(60);
+            Console.WriteLine(" After insert node the Binary Tree is:");
+            tree.inorder();
+        }
+    }
+}
